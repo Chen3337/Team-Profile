0b7057b679bd52377af4a08b8fe78d271d116140
zhen yong chen <dannychen111@yahoo.com>
2019-11-20 20:09:15 -0600

    templates are finished for now might change after app.js is finished

---
+++ app.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+// tested out example
+// const themainHtml = fs.readFileSync("templates/main.html", 'utf8',function(err){throw err});
+// const endingHtml = fs.readFileSync("templates/manager.html", 'utf8',function(err){throw err});
+// const wwww = endingHtml.repeat(5);
+// const res = themainHtml.replace("placeholder", wwww);
+// console.log(res);
+// fs.writeFile("index.html", res, function(err){if(err) throw err});
